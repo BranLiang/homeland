33f82d1b660b8caf5c5797543d5ae3a50a5466de
Bran Liang <lby89757@hotmail.com>
2017-11-18 20:12:36 +0800

    Config the scheduled sidekiq tasks

--- app/jobs/weekly_hot_topic_job.rb
+++ app/jobs/weekly_hot_topic_job.rb
@@ -1,6 +1,23 @@
 class WeeklyHotTopicJob
+  include TimeRangeHelper
   include Sidekiq::Worker
 
   def perform
+    target_topics.each do |topic|
+      ScoreCalculator.new(topic, ranges).calculate
+    end
+  end
+
+  private
+
+  def target_topics
+    Topic
+      .left_outer_joins(:topic_views)
+      .where("topics.replied_at >= ? OR topic_views.created_at >= ?", Time.now - 1.week, Time.now - 1.week)
+      .distinct
+  end
+
+  def ranges
+    @ranges ||= weekly_ranges
   end
 end
